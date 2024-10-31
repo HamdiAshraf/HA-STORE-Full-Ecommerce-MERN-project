26938b909e7c7200b76566159c0a765793eec96f
Hamdi Ashraf <hamdiashraf2000@gmail.com>
2024-10-31 07:48:18 +0100

    category page

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,6 +1,7 @@
 import {createBrowserRouter} from "react-router-dom";
 import App from "../App";
 import HomePage from "../pages/Home/HomePage";
+import CategoriesPage from "../pages/categories/CategoriesPage";
 
 
 const router = createBrowserRouter([
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
             {
                 path:"/",
                 element:<HomePage/>
+            },
+            {
+                path:"/categories/:categoryName",
+                element:<CategoriesPage />
             }
         ]
     }
