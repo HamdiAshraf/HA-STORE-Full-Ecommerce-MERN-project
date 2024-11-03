c2efdf54cc41b1df4577debe6ad98344ba9fa7f6
Hamdi Ashraf <hamdiashraf2000@gmail.com>
2024-11-03 08:36:26 +0100

    style the cart when you click on it

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import CartModal from "../pages/shop/CartModal";
 
 
 const Navbar = () => {
     const products = useSelector((state)=>state.cart.products);
+    const [isCartOpen,setIsCartOpen]=useState(false);
+    const handleCartToggle=()=>{
+        setIsCartOpen(!isCartOpen);
+    }
   return (
     <header className="fixed-nav-bar w-nav">
         <nav className="max-w-screen-2xl mx-auto px-4 flex justify-between items-center">
@@ -27,7 +33,7 @@ const Navbar = () => {
             </span>
 
             <span>
-                <button className="hover:text-primary">
+                <button  onClick={handleCartToggle} className="hover:text-primary">
                 <i className="ri-shopping-bag-line"></i>
                 <sup className="text-sm inline-block px-1.5 text-white rounded-full bg-primary text-center">{products.length}</sup>
                 </button>
@@ -40,6 +46,11 @@ const Navbar = () => {
             </span>
         </div>
         </nav>
+
+
+        {
+            isCartOpen && <CartModal products={products} isCartOpen={isCartOpen} onClose={handleCartToggle}/>
+        }
     </header>
   )
 }
---
+++ frontend/src/pages/shop/productDetails/OrderSummary.jsx
@@ -0,0 +1,11 @@
+
+
+const OrderSummary = () => {
+  return (
+    <div>
+      Order Summary
+    </div>
+  )
+}
+
+export default OrderSummary
